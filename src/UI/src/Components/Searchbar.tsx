a86d64e66615cb7e6fcd2ed956f31dac9ceec3ab
Tony Yuan <tonyyuan982@gmail.com>
2023-05-01 15:12:30 -0700

    search implementation with lunr

--- src/UI/src/Components/Searchbar.tsx
+++ src/UI/src/Components/Searchbar.tsx
@@ -1,7 +1,60 @@
-function Searchbar() {
+import { useState } from "react"
+import { useLunr } from 'react-lunr'
+import json from '../Assets/context.json'
+import lunr from "lunr"
+import { Formik, Form, Field } from "formik";
+
+const data = json;
+interface SearchResult {
+  title: string | null,
+  abstract: string,
+  runid: string
+  id: number
+}
+const store : Record<string, SearchResult> = {};
+const index = lunr(function () {
+  this.field("title");
+  this.field("abstract");
+  this.field("project_id");
+  this.field("runid");
+  this.ref("id");
+
+  for (let entry = 0; entry < data.length; entry++) {
+    this.add({
+      title: data[entry].title,
+      abstract: data[entry].abstract,
+      project_id: data[entry].project_id,
+      runid: data[entry].runid,
+      id: entry
+    });
+    console.log(data)
+
+    store[entry] = {
+      title: data[entry].title,
+      abstract: data[entry].abstract,
+      // project_id: data[entry].project_id,
+      runid: data[entry].runid,
+      id: entry
+    };
+  }
+});
+const Searchbar = () => {
+  const [query, setQuery] = useState("");
+  const results = useLunr(query, index, store);
+  console.log(results)
   return (
     <div id="searchbar">
-      <h2>Searchbar</h2>
+      <Formik
+        initialValues={{ query: '' }}
+        onSubmit={(values, { setSubmitting }) => {
+          setQuery(values.query)
+          setSubmitting(false)
+        }}
+      >
+        <Form>
+          <Field name="query" />
+        </Form>
+      </Formik>
     </div>
   )
 }
