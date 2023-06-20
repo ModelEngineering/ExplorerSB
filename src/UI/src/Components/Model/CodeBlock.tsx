f282a596b9ebea7b9ad08a1cee55572d5659061a
Tony Yuan <tonyyuan982@gmail.com>
2023-06-20 16:31:26 -0700

    copy button

--- src/UI/src/Components/Model/CodeBlock.tsx
+++ src/UI/src/Components/Model/CodeBlock.tsx
@@ -1,11 +1,31 @@
-import SyntaxHighlighter from 'react-syntax-highlighter';
+import { useState } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
 import { vscDarkPlus as stylesheet } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { BiCopy as CopyIcon, BiCheck as CheckIcon } from "react-icons/bi";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-const CodeBlock = ({codeString} : {codeString : string}) => {
+const CodeBlock = ({ codeString }: { codeString: string }) => {
+  const [copied, setCopied] = useState(false);
+  const copy = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2500);
+  };
   return (
-    <SyntaxHighlighter language="markup" style={stylesheet} showLineNumbers={true}>
-      {codeString}
-    </SyntaxHighlighter>
+    <div style={{position: "relative"}}>
+      <CopyToClipboard text={codeString} onCopy={copy}>
+        <button className="copy-button">{copied ? <CheckIcon /> : <CopyIcon />}</button>
+      </CopyToClipboard>
+      <SyntaxHighlighter
+        language="markup"
+        style={stylesheet}
+        showLineNumbers={true}
+        customStyle={{margin: "0"}}
+      >
+        {codeString}
+      </SyntaxHighlighter>
+    </div>
   );
 };
 
