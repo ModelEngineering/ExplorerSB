d898143245227f0d29c88cbf336c0f5480bf8fb8
Tony Yuan <tonyyuan982@gmail.com>
2023-06-24 18:56:10 -0700

    Header quick fix + add files

--- src/UI/src/Components/Header.tsx
+++ src/UI/src/Components/Header.tsx
@@ -4,7 +4,7 @@ import Searchbar from "./Searchbar";
 const Header = () => {
   return (
     <div id="header" className="flex-row">
-      <Link to="/" style={{ textDecoration: "inherit", color: "inherit" }}>
+      <Link to="/" style={{ textDecoration: "inherit", color: "inherit", flexShrink: 0 }}>
         <h1 id="logo">
           Reproducibility Portal
         </h1>
