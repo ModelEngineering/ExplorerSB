5dfd5e6f024d4fd982a337f070b76b582e434e87
Tony Yuan <Tonyyuan982@gmail.com>
2023-08-28 19:28:00 -0700

    added comments to pages

--- src/UI/app/model/[runid]/page.tsx
+++ src/UI/app/model/[runid]/page.tsx
@@ -1,7 +1,10 @@
 import Results from "@/components/results";
 
+// Define the runtime environment as 'edge'
 export const runtime = 'edge';
 
+// Define the default function component named "Model"
 export default function Model({ params }: { params: { runid: string } }) {
+  // Render the Results component with the "runid" parameter passed
   return <Results runid={params.runid} />;
 }
--- src/UI/app/page.tsx
+++ src/UI/app/page.tsx
@@ -1,11 +1,16 @@
 import Searchbar from "@/components/searchbar";
 
+// Define the default function component named "Home"
 export default function Home() {
   return (
+    // The main content of the home page
     <main className="flex min-h-screen flex-col items-center justify-normal gap-10 px-6 py-12 md:p-16 lg:p-24">
+      {/* Page title */}
       <h1 className="flex shrink-0 self-center px-1 text-center text-3xl font-bold text-inherit decoration-inherit md:text-4xl lg:text-5xl">
         Reproducibility Portal
       </h1>
+      
+      {/* Render the Searchbar component */}
       <Searchbar landing={true} />
     </main>
   );
