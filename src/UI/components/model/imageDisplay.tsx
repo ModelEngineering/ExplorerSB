0b873173295aaab9e55fc766ff90b051a6bf40e2
Tanya <tanyanaveen04@gmail.com>
2023-09-11 17:58:34 -0700

    added zoom in/out, reset buttons in image viewer

--- src/UI/components/model/imageDisplay.tsx
+++ src/UI/components/model/imageDisplay.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { TransformWrapper, TransformComponent, useControls } from "react-zoom-pan-pinch";
+import { BiZoomIn as ZoomIn, BiZoomOut as ZoomOut } from "react-icons/bi";
+import { GrPowerReset as Reset} from 'react-icons/gr';
 
 // Define the ImageDisplay component
 const ImageDisplay = ({
@@ -9,6 +11,21 @@ const ImageDisplay = ({
   fileName: string; // The name of the image file
   data: string; // The binary blob data of the image
 }) => {
+    // define component to create buttons for zooming in, zooming out, and resetting scale
+    const Controls = () => {
+      const { zoomIn, zoomOut, resetTransform } = useControls();
+      return (
+        <div className="m-auto inline-block">
+          <button title="Zoom In" className="h-8 w-8 m-2 rounded-lg bg-slate-300 p-2 text-center text-md hover:bg-slate-500"
+            onClick={() => zoomIn()}>{<ZoomIn/>}</button> 
+          <button title="Zoom Out" className="h-8 w-8 m-2 rounded-lg bg-slate-300 p-2 text-center text-md hover:bg-slate-500"
+            onClick={() => zoomOut()}>{<ZoomOut/>}</button>
+          <button title="Reset Zoom" className="h-8 w-8 m-2 rounded-lg bg-slate-300 p-2 text-center text-md hover:bg-slate-500"
+            onClick={() => resetTransform()}>{<Reset/>}</button>
+        </div>
+      );
+    };
+  
   return (
     // Wrap the image with TransformWrapper for zooming and panning functionality
     <TransformWrapper
@@ -17,6 +34,8 @@ const ImageDisplay = ({
         mode: 'reset' // Allow double click to reset the zoom
       }}
     >
+      {/* Display list of control buttons (zoom in/out, reset) */}
+      <Controls/>
       {/* Apply zooming and panning to the Image component */}
       <TransformComponent wrapperStyle={{ width: "100%", height: "100%" }} contentStyle={{ width: "100%", height: "100%" }}>
         {/* Display the image using the Next.js Image component */}
